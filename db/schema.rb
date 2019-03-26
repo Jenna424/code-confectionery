44051f1507a8644907215d0b0e8255340a3ae603
github username <github email address>
2019-03-26 11:13:38 -0400

    create ingredients DB table and Ingredient model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_26_151045) do
+ActiveRecord::Schema.define(version: 2019_03_26_151250) do
 
   create_table "cakes", force: :cascade do |t|
     t.string "structure", default: "layered"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2019_03_26_151045) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "flavor_combination"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
