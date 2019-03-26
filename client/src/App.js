7ee6f16d6d8f1d0480dbe1f56e159ef98b5e6456
github username <github email address>
2019-03-26 15:59:22 -0400

    import Navbar component and render in App render method

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Navbar />
         <Switch>
           <Route exact path="/" component={CakeConstructor} />
         </Switch>
