f7a1f8da8417120986382d6f9644fa0ebaa2c9d1
github username <github email address>
2019-03-27 14:31:42 -0400

    fix indentation of CakeConstructor component

--- client/src/containers/CakeConstructor.js
+++ client/src/containers/CakeConstructor.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 
 class CakeConstructor extends Component {
   render() {
-  	return (
-  	  <div>
-  	    CakeConstructor container class component will present a graphical representation of the cake that the user is currently customizing!
-  	  </div>
-  	)
+    return (
+      <div>
+        CakeConstructor container class component will present a graphical representation of the cake that the user is currently customizing!
+      </div>
+    )
   }
 }
 
