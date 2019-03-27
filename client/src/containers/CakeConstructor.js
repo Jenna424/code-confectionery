61947d9e112425616412dc38732546f9b95f13ea
github username <github email address>
2019-03-27 14:30:06 -0400

    stub out CakeConstructor container class React component

---
+++ client/src/containers/CakeConstructor.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CakeConstructor extends Component {
+  render() {
+  	return (
+  	  <div>
+  	    CakeConstructor container class component will present a graphical representation of the cake that the user is currently customizing!
+  	  </div>
+  	)
+  }
+}
+
+export default CakeConstructor;
