7d8255c6bf9c5df8eb5e39ae90ded465357902cb
github username <github email address>
2019-03-27 14:38:17 -0400

    import CakeConstructor container class component in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import CakeConstructor from './containers/CakeConstructor';
 
 class App extends Component {
   render() {
