462a5261d605077bded55bbfe3e0141a93de6e69
github username <github email address>
2019-03-26 21:02:44 -0400

    import Provider,createStore,applyMiddleware,combineReducers,thunk

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+
 import App from './App';
 
 const wrappedApp = (
