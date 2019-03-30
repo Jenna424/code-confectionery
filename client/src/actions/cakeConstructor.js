3e4305e5c3750ece5b72d58f76e4b355487c983c
github username <github email address>
2019-03-30 18:29:28 -0400

    add notes about stackLayer, unstackLayer action creators

--- client/src/actions/cakeConstructor.js
+++ client/src/actions/cakeConstructor.js
@@ -1,5 +1,5 @@
 import * as types from './actionTypes';
-
+// synchronous action creators:
 export const stackLayer = flavor => ({
   type: types.STACK_LAYER,
   flavor
@@ -9,3 +9,13 @@ export const unstackLayer = flavor => ({
   type: types.UNSTACK_LAYER,
   flavor
 })
+
+// stackLayer action creator arrow function accepts a string flavor as an argument
+// (a key/value pair property of a layer object)
+// and implicitly returns an action object of type 'STACK_LAYER'
+// which also contains a payload of the flavor of the layer being stacked on top of the cake
+
+// unstackLayer action creator arrow function accepts a string flavor as an argument
+// (a key/value pair property of a layer object)
+// and implicitly returns an action object of type 'UNSTACK_LAYER'
+// which also contains a payload of the flavor of the layer being unstacked from the cake
