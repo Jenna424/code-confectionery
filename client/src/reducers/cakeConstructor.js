7b1a60de101b17b70c64f4a1683e65537481b6e3
github username <github email address>
2019-03-30 19:18:03 -0400

    add notes to cakeConstructor reducer file

--- client/src/reducers/cakeConstructor.js
+++ client/src/reducers/cakeConstructor.js
@@ -1,8 +1,8 @@
 import * as types from '../actions/actionTypes';
 
 const initialState = {
-  layers: null,
-  cakeCost: 0,
+  layers: null, // initially set to null because layers will be fetched asynchronously
+  cakeCost: 0, // the cake board is free
   error: false
 }
 
@@ -13,3 +13,7 @@ const CAKE_COMPONENT_COSTS = {
   garnish: 2.5,
   topper: 1.5
 }
+
+// layers is an object representing all of the layers in the layer cake
+// each key in the layers object is a string flavor,
+// and its corresponding value is the number of layers in the cake with that particular flavor
