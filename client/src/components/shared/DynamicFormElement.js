799ef36d880f78e55e98b8a246b19927a2474563
github username <github email address>
2019-03-29 20:31:42 -0400

    stub out DynamicFormElement stateless functional component

---
+++ client/src/components/shared/DynamicFormElement.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DynamicFormElement = props => {
+  return (
+    <div>
+      <label>The actual label for the form element goes here.</label>
+      <p>Replace this paragraph with the form element</p>
+    </div>
+  )
+}
+
+export default DynamicFormElement;
