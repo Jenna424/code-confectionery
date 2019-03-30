1f03ebefc6bb820b25e5f1419107707d7c005fe4
github username <github email address>
2019-03-29 20:53:02 -0400

    add switch statement to switch string HTML tag type

--- client/src/components/shared/DynamicFormElement.js
+++ client/src/components/shared/DynamicFormElement.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 const DynamicFormElement = props => {
   let htmlTag = null;
-  
+
+  switch (props.stringHtmlTag) {
+  	case 'select':
+  	  htmlTag = <select>
+  	  break;
+  	case 'textarea':
+  	  htmlTag = <textarea />
+  	  break;
+  	default:
+  	  htmlTag = <input />
+  }
+
   return (
     <div>
       <label>The actual label for the form element goes here.</label>
