d6ca5ceca881ade3e4d720daae49050a9d462931
github username <github email address>
2019-03-29 20:37:28 -0400

    set htmlTag variable = null in DynamicFormElement

--- client/src/components/shared/DynamicFormElement.js
+++ client/src/components/shared/DynamicFormElement.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const DynamicFormElement = props => {
+  let htmlTag = null;
+  
   return (
     <div>
       <label>The actual label for the form element goes here.</label>
