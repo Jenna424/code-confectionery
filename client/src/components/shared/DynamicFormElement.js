a54198a6e1d92d045c82bf120edd2e2b2bcff434
github username <github email address>
2019-03-29 20:57:22 -0400

    replace <p> with {htmlTag} in JSX returned from DynamicFormElement

--- client/src/components/shared/DynamicFormElement.js
+++ client/src/components/shared/DynamicFormElement.js
@@ -22,7 +22,7 @@ const DynamicFormElement = props => {
   return (
     <div>
       <label>The actual label for the form element goes here.</label>
-      <p>Replace this paragraph with the form element</p>
+      {htmlTag}
     </div>
   )
 }
