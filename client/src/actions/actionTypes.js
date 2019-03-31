e1e90d6c8e8ec5cac00a533b698f2d72c4a75c1c
github username <github email address>
2019-03-31 09:39:56 -0400

    change SET_LAYERS to SET_LAYERS_SUCCESS

--- client/src/actions/actionTypes.js
+++ client/src/actions/actionTypes.js
@@ -1,5 +1,5 @@
 export const STACK_LAYER = 'STACK_LAYER';
 export const UNSTACK_LAYER = 'UNSTACK_LAYER';
 export const LOAD_LAYERS = 'LOAD_LAYERS';
-export const SET_LAYERS = 'SET_LAYERS';
+export const SET_LAYERS_SUCCESS = 'SET_LAYERS_SUCCESS';
 export const FETCH_LAYERS_FAILURE = 'FETCH_LAYERS_FAILURE';
