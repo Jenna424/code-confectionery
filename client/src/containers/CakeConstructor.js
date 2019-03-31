33c008ccab23f4224cd626bdc48d9e20701831a3
github username <github email address>
2019-03-31 10:59:13 -0400

    delete commented out import from actionTypes statement

--- client/src/containers/CakeConstructor.js
+++ client/src/containers/CakeConstructor.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-//import * as types from '../actions/actionTypes';
 import { stackLayer, unstackLayer } from '../actions/cakeConstructor';
 
 class CakeConstructor extends Component {
