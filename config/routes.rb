21827d9d6d850aa3242bd9b0486df3cf10df0c63
github username <github email address>
2019-03-31 11:50:53 -0400

    change resource from ingredients to layers in routing file

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :cakes, except: [:new, :edit]
-      resources :ingredients, except: [:new, :edit]
+      resources :layers, except: [:new, :edit]
     end
   end
 end
