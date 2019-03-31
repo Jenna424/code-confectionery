98ea3d5dfe219586e55d83db6e01efa648230c70
github username <github email address>
2019-03-31 15:22:55 -0400

    add scopes for :batter, :filling, :frosting in Layer model

--- app/models/layer.rb
+++ app/models/layer.rb
@@ -1,4 +1,8 @@
 class Layer < ApplicationRecord
+  scope :batter, -> { where pastry_part: 'batter' }
+  scope :filling, -> { where pastry_part: 'filling' }
+  scope :frosting, -> { where pastry_part: 'frosting' }
+  
   has_many :cake_layers
   has_many :cakes, through: :cake_layers
 end
