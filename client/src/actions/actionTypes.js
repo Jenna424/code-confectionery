f2fc212cdff042cf64c3cd62287fd0bb59f2d3f4
github username <github email address>
2019-03-31 09:37:04 -0400

    move SET_LAYERS right below LOAD_LAYERS constant

--- client/src/actions/actionTypes.js
+++ client/src/actions/actionTypes.js
@@ -1,5 +1,5 @@
 export const STACK_LAYER = 'STACK_LAYER';
 export const UNSTACK_LAYER = 'UNSTACK_LAYER';
 export const LOAD_LAYERS = 'LOAD_LAYERS';
-export const LOAD_LAYERS_FAILURE = 'LOAD_LAYERS_FAILURE';
 export const SET_LAYERS = 'SET_LAYERS';
+export const LOAD_LAYERS_FAILURE = 'LOAD_LAYERS_FAILURE';
