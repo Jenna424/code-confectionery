13416be1d8395948bacdae0f3c165c2d7d86485b
github username <github email address>
2019-03-31 11:23:12 -0400

    call this.props.onLoadLayers() in componentDidMount lifecycle method

--- client/src/containers/CakeConstructor.js
+++ client/src/containers/CakeConstructor.js
@@ -3,6 +3,11 @@ import { connect } from 'react-redux';
 import { loadLayers, stackLayer, unstackLayer } from '../actions/cakeConstructor';
 
 class CakeConstructor extends Component {
+
+  componentDidMount() {
+    this.props.onLoadLayers(); // executing action-dispatching arrow function (received from mapDispatchToProps) which, in turn, calls dispatch() to call function returned by loadLayers asynchronous action creator function, to hopefully successfully fetch layers from Rails API
+  }
+
   render() {
     return (
       <div>
@@ -33,6 +38,6 @@ export default connect(mapStateToProps, mapDispatchToProps)(CakeConstructor);
 // mapDispatchToProps stores an arrow function that accepts the dispatch function as an argument
 // It implicitly returns a JS object with props function mappings.
 // That is, the keys in the returned object are the names of action-dispatching arrow functions
-// whose corresponding values are the callback functions that, when triggered, 
+// whose corresponding values are the callback arrow functions that, when triggered, 
 // will invoke the dispatch function with the action object returned by invoking the action creator function
 */}
