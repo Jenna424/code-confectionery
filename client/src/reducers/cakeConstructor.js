b5a14415a8db4da5d11fb3bb5e86714669a258a7
github username <github email address>
2019-03-31 07:49:16 -0400

    add case types.LOAD_LAYERS_FAILURE to cakeConstructor reducer

--- client/src/reducers/cakeConstructor.js
+++ client/src/reducers/cakeConstructor.js
@@ -40,6 +40,11 @@ export default (state = initialState, action) => {
         layers: action.layers, // loadLayersSuccess action creator function returned an action object w/ layers key pointing to layers payload (an array of layer objects that I got back in JSON response from server)
         error: false // set error to false to clear it in case we previously had an error that was since resolved
       };
+    case types.LOAD_LAYERS_FAILURE:
+      return {
+        ...state,
+        error: true
+      };
   }
 }
 
