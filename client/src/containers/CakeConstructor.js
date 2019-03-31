7b7e655947646ccd63302c4bfaa01965a2f5df11
github username <github email address>
2019-03-31 12:22:20 -0400

    fix spacing in CakeConstructor

--- client/src/containers/CakeConstructor.js
+++ client/src/containers/CakeConstructor.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { loadLayers, stackLayer, unstackLayer } from '../actions/cakeConstructor';
 
 class CakeConstructor extends Component {
-
+  
   componentDidMount() {
     this.props.onLoadLayers(); // executing action-dispatching arrow function (received from mapDispatchToProps) which, in turn, calls dispatch() to call function returned by loadLayers asynchronous action creator function, to hopefully successfully fetch layers from Rails API
   }
@@ -34,7 +34,6 @@ export default connect(mapStateToProps, mapDispatchToProps)(CakeConstructor);
 // mapStateToProps stores an arrow function that accepts the entire Redux store state object as an argument.
 // It implicitly returns a JS object with the portion of the Redux store state that we want to update.
 // and this is used to determine what, if anything, should be re-rendered depending on if anything has changed
-
 // mapDispatchToProps stores an arrow function that accepts the dispatch function as an argument
 // It implicitly returns a JS object with props function mappings.
 // That is, the keys in the returned object are the names of action-dispatching arrow functions
