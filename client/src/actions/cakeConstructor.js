847c9718b7b02702a9fefdb8197f3e977dc3ed1d
github username <github email address>
2019-03-31 09:41:41 -0400

    change setLayers to setLayersSuccess; define loadLayers

--- client/src/actions/cakeConstructor.js
+++ client/src/actions/cakeConstructor.js
@@ -12,22 +12,24 @@ export const unstackLayer = layer => ({
   layer
 })
 
-export const setLayers = layers => ({
-  type: types.SET_LAYERS,
+export const setLayersSuccess = layers => ({
+  type: types.SET_LAYERS_SUCCESS,
   layers
 })
-// loadLayersSuccess is an asynchronous action creator function, made possible by redux-thunk
-export const loadLayersSuccess = () => {
+// loadLayers is an asynchronous action creator function, made possible by redux-thunk
+export const loadLayers = () => {
+  let requestObject = {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include'
+  }
   return dispatch => {
-  	// Execute async code here to fetch layers from my Rails API backend server.
-  	// After the async code is done and I've gotten a JSON response back from my server,
-  	// (which contains the array of layers objects)
-  	// I will invoke dispatch() function to dispatch the action object
-  	// that is returned by calling a synchronous action creator function called setLayers, 
-  	// which I'm about to define!
+    return fetch(`${BASE_URL}/layers`, requestObject) // make async call to fetch layers from my Rails API backend server
+      .then(response => response.json()) // handle promises. Once promises are resolved, after the async code is done and I've gotten a successful JSON response back from my server, which contains the array of layer objects,
+      .then(layers => dispatch(setLayersSuccess(layers))) // dispatch the action object that is returned by calling synchronous action creator function setLayersSuccess with this array of layer objects as its argument.
+      .catch(error => dispatch(fetchLayersFailure())) // However, if my request to fetch layers from my Rails server failed, I want to adjust my error key = true in Redux store state. To this end, I dispatch the action object that is returned by calling synchronous action creator function fetchLayersFailure()
   };
 };
-
 // stackLayer action creator arrow function accepts a layer object as an argument
 // and implicitly returns an action object of type 'STACK_LAYER'
 // which also contains a payload of the layer being stacked on top of the cake
@@ -36,17 +38,20 @@ export const loadLayersSuccess = () => {
 // and implicitly returns an action object of type 'UNSTACK_LAYER'
 // which also contains a payload of the layer being unstacked from the cake
 
-// loadLayersSuccess asynchronous action creator function will be invoked to initialize, 
+// loadLayers asynchronous action creator function will be invoked to initialize, 
 // or load, the layers that I can use in the CakeConstructor container class component.
-// loadLayersSuccess asynchronous action creator function returns a function 
+// loadLayers asynchronous action creator function returns a function 
 // that receives the dispatch function as its argument.
 // Inside the body of the returned function, I execute asynchronous code to fetch layers from my Rails API backend server.
-// Once that async code is done and I've received a JSON response from my server,
+// Once that async code is done and I've received a successful JSON response from my server,
 // which contains the array of layer objects,
 // the dispatch function is called inside the body of the returned function
-// to dispatch the action object which was returned by invoking my synchronous action creator function, setLayers.
-// setLayers synchronous action creator function accepts the array of layer objects 
+// to dispatch the action object which was returned by invoking my synchronous action creator function, setLayersSuccess.
+// setLayersSuccess synchronous action creator function accepts the array of layer objects 
 // (that I got back in the JSON response from my Rails server) as an argument.
-// setLayers action creator function implicitly returns an action object
-// that has a type key set = to 'SET_LAYERS' and a layers key set = to the layers payload, i.e.,
+// setLayersSuccess action creator function implicitly returns an action object
+// that has a type key set = to 'SET_LAYERS_SUCCESS' and a layers key set = to the layers payload, i.e.,
 // the array of layer objects.
+// However, if my request to fetch layers from my Rails server failed, 
+// I want to adjust my error key = true in Redux store state. 
+// To this end, I dispatch the action object that is returned by calling synchronous action creator function fetchLayersFailure()
