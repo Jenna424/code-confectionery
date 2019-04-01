4e77247c34c1fabeea930b94fb37ae1ecb89d501
github username <github email address>
2019-04-01 15:02:43 -0400

    stub out LayerLevers stateless functional component

---
+++ client/src/components/LayerLevers/LayerLevers.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LayerLevers = props => (
+  <div>
+    <p>I will render buttons to add and remove layers from the cake here.</p>
+  </div>
+)
+
+export default LayerLevers;
