8cc70a84012fef6b8b7543612070fd2e7fa91bae
github username <github email address>
2019-04-01 15:10:23 -0400

    destructure props in LayerLevers component

--- client/src/components/LayerLevers/LayerLevers.js
+++ client/src/components/LayerLevers/LayerLevers.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const LayerLevers = props => (
+const LayerLevers = ({ batterLayers, fillingLayers, onStackLayer, onUnstackLayer, dataToDisable }) => (
   <div>
     <p>I will render buttons to add and remove layers from the cake here.</p>
   </div>
