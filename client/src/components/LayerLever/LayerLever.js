de3b7297978500a9dc8b2d5995b67414b8692f08
github username <github email address>
2019-04-02 11:39:53 -0400

    remove 'on' from action-dispatching fns; change onClick to anonymous arrow fns

--- client/src/components/LayerLever/LayerLever.js
+++ client/src/components/LayerLever/LayerLever.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import styles from './LayerLever.module.css';
 
-const LayerLever = ({ leverLabel, lastLayerLevered, layer, cakeLayout, onStackLayer, onUnstackLayer, disabled }) => (
+const LayerLever = ({ leverLabel, lastLayerLevered, layer, cakeLayout, stackLayer, unstackLayer, disabled }) => (
   <div>
     <div className="ui buttons">
-      <button onClick={onStackLayer} className="tiny ui compact button">+</button>
+      <button onClick={() => stackLayer(layer)} className="tiny ui compact button">+</button>
       <div className="or"></div>
-      <button onClick={onUnstackLayer} className="tiny ui compact pink button">-</button>
+      <button onClick={() => unstackLayer(layer)} className="tiny ui compact pink button">-</button>
     </div>
     <label>&nbsp;{leverLabel}</label>
   </div>
