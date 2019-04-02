10670cd8a12bee5c5b583d85636ed83feeab0403
github username <github email address>
2019-04-02 19:25:49 -0400

    define bakeBatterLayer in Cake.js; remove Cake component

--- client/src/components/Cake/Cake.js
+++ client/src/components/Cake/Cake.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import styles from './Cake.module.css';
 
-const Cake = () => {
+const bakeBatterLayer = batterFlavor => {
+  const cakeBatterClasses = [styles.batter];
+
+  switch (batterFlavor) {
+  	case 'red velvet':
+  	  cakeBatterClasses.push(styles.redVelvet);
+  	  break;
+  	case 'carrot':
+  	  cakeBatterClasses.push(styles.carrot);
+  	  break;
+  	default:
+  	  cakeBatterClasses.push(styles.mysteryFlavor)
+  }
+
   return (
-    <div className={styles.cakeContainer}>
-      <div className={styles.cakeBoard}></div>
-    </div>
+  	<div data-flavor={batterFlavor} className={cakeBatterClasses.join(' ')}></div>
   )
 }
-
-export default Cake;
