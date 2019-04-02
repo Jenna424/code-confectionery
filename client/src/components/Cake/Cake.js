5fbd0012d4d72220386251b9d06146cbfef2f653
github username <github email address>
2019-04-02 19:27:22 -0400

    fix indentation of bakeBatterLayer in Cake.js

--- client/src/components/Cake/Cake.js
+++ client/src/components/Cake/Cake.js
@@ -5,17 +5,17 @@ const bakeBatterLayer = batterFlavor => {
   const cakeBatterClasses = [styles.batter];
 
   switch (batterFlavor) {
-  	case 'red velvet':
-  	  cakeBatterClasses.push(styles.redVelvet);
-  	  break;
-  	case 'carrot':
-  	  cakeBatterClasses.push(styles.carrot);
-  	  break;
-  	default:
-  	  cakeBatterClasses.push(styles.mysteryFlavor)
+    case 'red velvet':
+      cakeBatterClasses.push(styles.redVelvet);
+      break;
+    case 'carrot':
+      cakeBatterClasses.push(styles.carrot);
+      break;
+    default:
+      cakeBatterClasses.push(styles.mysteryFlavor)
   }
 
   return (
-  	<div data-flavor={batterFlavor} className={cakeBatterClasses.join(' ')}></div>
+    <div data-flavor={batterFlavor} className={cakeBatterClasses.join(' ')}></div>
   )
 }
