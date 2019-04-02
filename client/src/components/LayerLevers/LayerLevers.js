c121a6829f9b878e14dd14a16aaed78c18f71a31
github username <github email address>
2019-04-01 20:21:51 -0400

    add import statement to import LayerLever in LayerLevers

--- client/src/components/LayerLevers/LayerLevers.js
+++ client/src/components/LayerLevers/LayerLevers.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import LayerLever from '../LayerLever/LayerLever';
 
 const LayerLevers = ({ batterLayers, fillingLayers, lastLayerLevered, onStackLayer, onUnstackLayer, cakeLayout }) => (
   <div>
