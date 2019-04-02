2f2cd482bc285ffe3a02c2d293472f6d0b5bb869
github username <github email address>
2019-04-02 11:45:23 -0400

    change leverLabel prop to flavorLabel prop in LayerLever.js

--- client/src/components/LayerLever/LayerLever.js
+++ client/src/components/LayerLever/LayerLever.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from './LayerLever.module.css';
 
-const LayerLever = ({ leverLabel, lastLayerLevered, layer, cakeLayout, stackLayer, unstackLayer, disabled }) => (
+const LayerLever = ({ flavorLabel, lastLayerLevered, layer, cakeLayout, stackLayer, unstackLayer, disabled }) => (
   <div>
     <div className="ui buttons">
       <button onClick={() => stackLayer(layer)} className="tiny ui compact button">+</button>
       <div className="or"></div>
       <button onClick={() => unstackLayer(layer)} className="tiny ui compact pink button">-</button>
     </div>
-    <label>&nbsp;{leverLabel}</label>
+    <label>&nbsp;{flavorLabel}</label>
   </div>
 )
 
