bc489583da96aee17b3e119196a913280e3ee225
github username <github email address>
2019-04-01 20:05:12 -0400

    italicize <p>; add italicized <p> and iteration for fillings

--- client/src/components/LayerLevers/LayerLevers.js
+++ client/src/components/LayerLevers/LayerLevers.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LayerLevers = ({ batterLayers, fillingLayers, lastLayerLevered, onStackLayer, onUnstackLayer, cakeLayout }) => (
   <div>
-    <p>Available Cake Batter Flavors</p>
+    <p><em>Available Cake Batter Flavors</em></p>
     {batterLayers.map(layerObject =>
       <LayerLever
         key={layerObject.id}
@@ -12,7 +12,16 @@ const LayerLevers = ({ batterLayers, fillingLayers, lastLayerLevered, onStackLay
         disabled={lastLayerLevered === layerObject.pastry_part || cakeLayout[layerObject.flavor] === 0}
       />
     )}
-    
+    <p><em>Available Filling Flavors</em></p>
+    {fillingLayers.map(layerObject =>
+      <LayerLever
+        key={layerObject.id}
+        leverLabel={layerObject.flavor}
+        onStackLayer={() => onStackLayer(layerObject)}
+        onUnstackLayer={() => onUnstackLayer(layerObject)}
+        disabled={lastLayerLevered === layerObject.pastry_part || cakeLayout[layerObject.flavor] === 0}
+      />
+    )}
   </div>
 )
 
