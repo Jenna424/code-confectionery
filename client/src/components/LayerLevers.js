1372af207b18c062410e024c0249c3d453f6290c
github username <github email address>
2019-04-03 13:59:52 -0400

    stub out LayerLevers stateless functional component

---
+++ client/src/components/LayerLevers.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react';
+
+const LayerLevers = props => (
+  <Fragment>
+    <p>Buttons for adding and removing layers will be displayed here</p>
+  </Fragment>
+)
+
+export default LayerLevers;
