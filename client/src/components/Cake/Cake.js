4af29bd7736e9ce76ad61c8f5a1ae60e506fc791
github username <github email address>
2019-04-03 15:45:17 -0400

    define Cake component w/ destructured props & cakeLayers variable

--- client/src/components/Cake/Cake.js
+++ client/src/components/Cake/Cake.js
@@ -1,3 +1,9 @@
 import React from 'react';
 
+const Cake = ({ cakeLayout }) => {
+  const cakeLayers = cakeLayout.map(cakeObject => 
+    <CakeLayer flavor={cakeObject.flavor} />
+  )
+}
+
 export default Cake;
