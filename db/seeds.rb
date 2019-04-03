4697a0d6b88ea18e5311548d0b980cafef5ade47
github username <github email address>
2019-04-03 11:05:25 -0400

    add more batter layers to db/seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
 layers = Layer.create([
   { flavor: 'Vanilla Sponge', pastry_part: 'batter' },
   { flavor: 'Chocolate Sponge', pastry_part: 'batter' },
+  { flavor: 'Devil\'s Food Cake', pastry_part: 'batter' },
+  { flavor: 'Strawberry Shortcake', pastry_part: 'batter' },
+  { flavor: 'Peanut Butter', pastry_part: 'batter' },
+  { flavor: 'Pumpkin', pastry_part: 'batter' },
+  { flavor: 'Gingerbread', pastry_part: 'batter' },
   { flavor: 'Red Velvet', pastry_part: 'batter' },
   { flavor: 'Carrot', pastry_part: 'batter' },
   { flavor: 'Lemon', pastry_part: 'batter' },
+  { flavor: 'Marble', pastry_part: 'batter' },
+  { flavor: 'Buttercream', pastry_part: 'batter' },
   { flavor: 'Chocolate Ganache', pastry_part: 'filling' },
   { flavor: 'Bavarian Cream', pastry_part: 'filling' },
   { flavor: 'Dulce de Leche', pastry_part: 'filling' }
