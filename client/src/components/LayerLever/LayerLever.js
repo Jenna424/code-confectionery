b52e4cb3eefd7eafde59648a4d89ac78dcbd86a2
github username <github email address>
2019-04-03 08:21:13 -0400

    change flavorLabel to label in LayerLever presentational component

--- client/src/components/LayerLever/LayerLever.js
+++ client/src/components/LayerLever/LayerLever.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from './LayerLever.module.css';
 
-const LayerLever = ({ stackLayer, unstackLayer, disableStack, disableUnstack, flavorLabel }) => (
+const LayerLever = ({ stackLayer, unstackLayer, disableStack, disableUnstack, label }) => (
   <div>
     <div className="ui buttons">
       <button onClick={stackLayer} className="tiny ui compact button" disabled={disableStack}>+</button>
       <div className="or"></div>
       <button onClick={unstackLayer} className="tiny ui compact pink button" disabled={disableUnstack}>-</button>
     </div>
-    <label>&nbsp;{flavorLabel}</label>
+    <label>&nbsp;{label}</label>
   </div>
 )
 
