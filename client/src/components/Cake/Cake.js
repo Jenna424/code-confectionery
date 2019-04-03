614b841f93fc3a448e49c863da486431d98e04bd
github username <github email address>
2019-04-03 15:42:14 -0400

    stub out Cake presentational component

--- client/src/components/Cake/Cake.js
+++ client/src/components/Cake/Cake.js
@@ -1,33 +1,13 @@
 import React from 'react';
-import styles from './Cake.module.css';
 
-const bakeBatterLayer = batterFlavor => {
-  const cakeBatterClasses = [styles.batter];
-
-  switch (batterFlavor) {
-    case 'red velvet':
-      cakeBatterClasses.push(styles.redVelvet);
-      break;
-    case 'carrot':
-      cakeBatterClasses.push(styles.carrot);
-      break;
-    default:
-      cakeBatterClasses.push(styles.mysteryFlavor)
-  }
-
-  return (
-    <div data-flavor={batterFlavor} className={cakeBatterClasses.join(' ')}></div>
+const Cake = ({ cakeLayout }) => {
+  const cakeLayers = cakeLayout.map(cakeObject => 
+  	<CakeLayer flavor={cakeObject.flavor} />
   )
-}
 
-const Cake = () => {
   return (
-    <div className={styles.cakeContainer}>
-      {bakeBatterLayer('red velvet')}
-      {bakeBatterLayer('mint')}
-      {bakeBatterLayer('keylime pie')}
-      {bakeBatterLayer('carrot')}
-      <div className={styles.cakeBoard}></div>
+    <div>
+      <p>Graphical representation of the layer cake currently being created goes here</p>
     </div>
   )
 }
