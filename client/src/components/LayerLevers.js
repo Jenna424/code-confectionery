a1b08bf9da1150a237c3c19594c32a3d1f78a6cc
github username <github email address>
2019-04-03 14:04:18 -0400

    add + button to add a layer

--- client/src/components/LayerLevers.js
+++ client/src/components/LayerLevers.js
@@ -3,6 +3,7 @@ import React, { Fragment } from 'react';
 const LayerLevers = ({ stackLayer, unstackLayer, disableStack, disableUnstack, label }) => (
   <Fragment>
     <div className="ui buttons">
+      <button onClick={stackLayer} className="tiny ui compact button" disabled={disableStack}>+</button>
     </div>
   </Fragment>
 )
