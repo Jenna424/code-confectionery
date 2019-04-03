3d5d92cf471690d4b421a7d739f923a45108be99
github username <github email address>
2019-04-02 22:16:31 -0400

    edit onClick and disabled props in buttons; edit props destructuring

--- client/src/components/LayerLever/LayerLever.js
+++ client/src/components/LayerLever/LayerLever.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import styles from './LayerLever.module.css';
 
-const LayerLever = ({ flavorLabel, lastLayerLevered, layer, cakeLayout, stackLayer, unstackLayer, disabled }) => (
+const LayerLever = ({ stackLayer, unstackLayer, disableStack, disableUnstack, flavorLabel }) => (
   <div>
     <div className="ui buttons">
-      <button onClick={() => stackLayer(layer)} className="tiny ui compact button">+</button>
+      <button onClick={stackLayer} className="tiny ui compact button" disabled={disableStack}>+</button>
       <div className="or"></div>
-      <button onClick={() => unstackLayer(layer)} className="tiny ui compact pink button">-</button>
+      <button onClick={unstackLayer} className="tiny ui compact pink button" disabled={disableUnstack}>-</button>
     </div>
     <label>&nbsp;{flavorLabel}</label>
   </div>
