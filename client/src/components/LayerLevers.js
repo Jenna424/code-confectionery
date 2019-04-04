03ac31af454cc504e72540c8ac997bc29ab84b1f
github username <github email address>
2019-04-04 08:57:13 -0400

    delete <div> styling; add 2 <br />s

--- client/src/components/LayerLevers.js
+++ client/src/components/LayerLevers.js
@@ -2,12 +2,13 @@ import React, { Fragment } from 'react';
 
 const LayerLevers = ({ stackLayer, unstackLayer, disableStack, disableUnstack, label }) => (
   <Fragment>
-    <div style={{display: 'block'}}>{label}</div>
+    <div>{label}</div>
     <div className="ui buttons">
       <button onClick={stackLayer} className="tiny ui compact button" disabled={disableStack}>+</button>
       <div className="or"></div>
       <button onClick={unstackLayer} className="tiny ui compact pink button" disabled={disableUnstack}>-</button>
     </div>
+    <br /><br />
   </Fragment>
 )
 
