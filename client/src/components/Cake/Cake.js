dd718e3bf80e04c93e122e960d1de0584b0d6cb9
github username <github email address>
2019-04-04 11:19:27 -0400

    define defaultFilling variable & delete if logic in Cake.js

--- client/src/components/Cake/Cake.js
+++ client/src/components/Cake/Cake.js
@@ -3,20 +3,11 @@ import styles from './Cake.module.css';
 import CakeLayer from './CakeLayer';
 
 const Cake = ({ cakeLayout, error }) => {
+  let defaultFilling;
+
   let cakeLayers = cakeLayout.map(cakeObject => 
     <CakeLayer flavor={cakeObject.flavor} />
   )
-
-  if (!cakeLayers.length) {
-    cakeLayers = (
-      <Fragment>
-        <div className={styles.standardSpongeTop}></div>
-        <p><em>Swap this standard slab of spongecake for your custom cake confection!</em></p>
-        <p><em>Please prepare your pastry by adding alternate layers of batter and filling</em></p>
-        <div className={styles.standardSpongeBase}></div>
-      </Fragment>
-    )
-  }
 
   let cakeClass = cakeLayout.length ? styles.cakeContainer : styles.standardSponge
 
