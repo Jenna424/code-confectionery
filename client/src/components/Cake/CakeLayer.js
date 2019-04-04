6ac5b82c1bdf1bf4588fb73b96896ff7bf305331
github username <github email address>
2019-04-04 14:47:04 -0400

    define styleBatter arrow function w/ switch statement

--- client/src/components/Cake/CakeLayer.js
+++ client/src/components/Cake/CakeLayer.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import styles from './Cake.module.css';
 
 class CakeLayer extends Component {
+  styleBatter= flavor => {
+    let batterClasses = [styles.batter];
+    switch (flavor) {
+      case ('Red Velvet'):
+        batterClasses.push(styles.redVelvet);
+        break;
+      case ('Carrot'):
+        batterClasses.push(styles.carrot)
+        break;
+      default:
+        batterClasses.push(styles.mysteryFlavor)
+    }
+    return batterClasses;
+  }
 }
 
 export default CakeLayer;
