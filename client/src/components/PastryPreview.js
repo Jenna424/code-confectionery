25624aaca57e07a8c10f93dab5a3bccee0b55ca7
github username <github email address>
2019-04-05 13:00:56 -0400

    define PastryPreview presentational component

---
+++ client/src/components/PastryPreview.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const PastryPreview = () => {
+  return (
+    <Fragment>
+      <h3><em>Preview your pastry prior to purchase</em></h3>
+    </Fragment>
+  )
+}
+
+export default PastryPreview;
