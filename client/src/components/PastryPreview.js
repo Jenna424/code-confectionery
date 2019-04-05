4d4aafe013ef017992ceb722651dc74ecc91ddf2
github username <github email address>
2019-04-05 13:07:49 -0400

    add <p> to PastryPreview JSX returned

--- client/src/components/PastryPreview.js
+++ client/src/components/PastryPreview.js
@@ -4,6 +4,7 @@ const PastryPreview = () => {
   return (
     <Fragment>
       <h3><em>Preview your pastry prior to purchase</em></h3>
+      <p>Your cake contains the following layers, from top to bottom:</p>
     </Fragment>
   )
 }
