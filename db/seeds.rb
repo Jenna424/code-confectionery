5b9ffee274f2c65a3902cfbdcb3843b053b9dcb1
github username <github email address>
2019-04-04 21:10:00 -0400

    move Keylime Pie layer; add Strawberry Jam in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,15 @@ layers = Layer.create([
   { flavor: 'Banana', pastry_part: 'batter'},
   { flavor: 'Marble', pastry_part: 'batter' },
   { flavor: 'Coffee', pastry_part: 'batter' },
-  { flavor: 'Keylime Pie', pastry_part: 'filling' },
   { flavor: 'Buttercream', pastry_part: 'filling' },
   { flavor: 'Chocolate Ganache', pastry_part: 'filling' },
   { flavor: 'Bavarian Cream', pastry_part: 'filling' },
   { flavor: 'Dulce de Leche', pastry_part: 'filling' },
   { flavor: 'Whipped Cream', pastry_part: 'filling' },
+  { flavor: 'Keylime Pie', pastry_part: 'filling' },
   { flavor: 'Cream Cheese Frosting', pastry_part: 'filling' },
   { flavor: 'Raspberry Jam', pastry_part: 'filling' },
   { flavor: 'Blueberry Jam', pastry_part: 'filling' },
+  { flavor: 'Strawberry Jam', pastry_part: 'filling' },
   { flavor: 'Apricot Preserves', pastry_part: 'filling' }
 ])
