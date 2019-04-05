da312d224028083c4961b1c8d39f59bb3b2b6b21
github username <github email address>
2019-04-05 16:14:44 -0400

    define containsFilling variable in PastryPartsPreview

--- client/src/components/PastryPartsPreview.js
+++ client/src/components/PastryPartsPreview.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react';
 
 const PastryPartsPreview = ({ cakeLayout, cakeCost }) => {
+  const containsFilling = cakeLayout.filter(layerObject => layerObject.pastry_part === 'filling').length > 0;
+
   return (
     <Fragment>
       <h3><em>Preview Your Pastry Prior to Purchase</em></h3>
