24179d576aafee37bf8219415a9b6e7351fc8ff3
github username <github email address>
2019-04-05 13:12:18 -0400

    add cakeLayout props;define layerLis;edit <p>; add <ul>

--- client/src/components/PastryPreview.js
+++ client/src/components/PastryPreview.js
@@ -1,10 +1,14 @@
 import React, { Fragment } from 'react';
 
-const PastryPreview = () => {
+const PastryPreview = ({ cakeLayout }) => {
+  const layerLis = cakeLayout.map(layerObject => <li key={layerObject.id}>{layerObject.flavor}</li>)
   return (
     <Fragment>
       <h3><em>Preview your pastry prior to purchase</em></h3>
-      <p>Your cake contains the following layers, from top to bottom:</p>
+      <p>Your cake contains the following layers, listed from top to bottom:</p>
+      <ul>
+        {layerLis}
+      </ul>
     </Fragment>
   )
 }
