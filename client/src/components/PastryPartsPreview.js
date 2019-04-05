1bcf8602b5ad224fd0c2437c7a4b72b9cd706f7b
github username <github email address>
2019-04-05 16:12:47 -0400

    change PastryPreview to PastryPartsPreview; redefine component

---
+++ client/src/components/PastryPartsPreview.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const PastryPartsPreview = ({ cakeLayout, cakeCost }) => {
+  return (
+    <Fragment>
+      <h3><em>Preview Your Pastry Prior to Purchase</em></h3>
+    </Fragment>
+  )
+}
+
+export default PastryPartsPreview;
