436514c9e82b84447cc2bae87b47e8d72d05799d
github username <github email address>
2019-04-04 21:04:03 -0400

    edit batter and filling layer dummy data in seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,22 @@
 layers = Layer.create([
   { flavor: 'Vanilla Sponge', pastry_part: 'batter' },
   { flavor: 'Chocolate Sponge', pastry_part: 'batter' },
-  { flavor: 'Devil\'s Food Cake', pastry_part: 'batter' },
   { flavor: 'Strawberry Shortcake', pastry_part: 'batter' },
-  { flavor: 'Peanut Butter', pastry_part: 'batter' },
   { flavor: 'Pumpkin', pastry_part: 'batter' },
   { flavor: 'Gingerbread', pastry_part: 'batter' },
   { flavor: 'Red Velvet', pastry_part: 'batter' },
   { flavor: 'Carrot', pastry_part: 'batter' },
   { flavor: 'Lemon', pastry_part: 'batter' },
+  { flavor: 'Banana', pastry_part: 'batter'},
   { flavor: 'Marble', pastry_part: 'batter' },
-  { flavor: 'Buttercream', pastry_part: 'batter' },
+  { flavor: 'Coffee Cake', pastry_part: 'batter' },
+  { flavor: 'Buttercream', pastry_part: 'filling' },
   { flavor: 'Chocolate Ganache', pastry_part: 'filling' },
   { flavor: 'Bavarian Cream', pastry_part: 'filling' },
-  { flavor: 'Dulce de Leche', pastry_part: 'filling' }
+  { flavor: 'Dulce de Leche', pastry_part: 'filling' },
+  { flavor: 'Whipped Cream', pastry_part: 'filling' },
+  { flavor: 'Cream Cheese Frosting', pastry_part: 'filling' },
+  { flavor: 'Raspberry Jam', pastry_part: 'filling' },
+  { flavor: 'Blueberry Jam', pastry_part: 'filling' },
+  { flavor: 'Apricot Preserves', pastry_part: 'filling' }
 ])
