47a2d1948c8e2a9d96e362e2807c6fdb0a05d50d
github username <github email address>
2019-04-05 14:54:32 -0400

    capitalize letters in italicized <h3> in PastryPreview

--- client/src/components/PastryPreview.js
+++ client/src/components/PastryPreview.js
@@ -4,7 +4,7 @@ const PastryPreview = ({ cakeLayout }) => {
   const layerLis = cakeLayout.map(layerObject => <li key={layerObject.id}>{layerObject.flavor}</li>)
   return (
     <Fragment>
-      <h3><em>Preview your pastry prior to purchase</em></h3>
+      <h3><em>Preview Your Pastry Prior to Purchase</em></h3>
       <p>Your cake contains the following layers, listed from top to bottom:</p>
       <ul>
         {layerLis}
