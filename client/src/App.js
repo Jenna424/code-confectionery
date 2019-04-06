9cc699668af8d21929bf8eabcacecdcced68f8c6
github username <github email address>
2019-04-06 12:48:14 -0400

    fix import statement w/ CakeCustomizerForm/ subfolder

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import CakeConstructor from './containers/CakeConstructor/CakeConstructor';
-import CakeCustomizerForm from './containers/CakeCustomizerForm';
+import CakeCustomizerForm from './containers/CakeCustomizerForm/CakeCustomizerForm';
 
 class App extends Component {
   render() {
