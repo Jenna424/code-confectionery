82b589434910ad05f2d989e5b70de348c1bd1056
github username <github email address>
2019-04-06 17:13:13 -0400

    add props,vars,if logic,className,validationErrorMessage,label

--- client/src/components/shared/DynamicFormElement/DynamicFormElement.js
+++ client/src/components/shared/DynamicFormElement/DynamicFormElement.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import styles from './DynamicFormElement.module.css';
 
-const DynamicFormElement = ({ stringTag, tagContents, value, formIsInvalid, handleOnChange }) => {
+const DynamicFormElement = ({ stringTag, tagContents, labelText, value, mustValidate, isInvalid, handleOnChange, interactedWith }) => {
+  const tagClassesArray = [styles.genericTag];
+  let validationErrorMessage = null;
+
+  if (interactedWith && mustValidate && isInvalid) {
+    tagClassesArray.push(styles.validationErrorMessage)
+  }
+  
+  if (interactedWith && isInvalid) {
+    validationErrorMessage = <span style={{color: 'red'}}>{labelText} must be valid. Please try again!</span>
+  }
+
   let htmlTag = null;
+  
 
   switch (stringTag) {
     case 'select':
       htmlTag = (
         <select
+          className={tagClassesArray.join(' ')}
           value={value}
           onChange={handleOnChange}>
           {tagContents.optionsArray.map(optionObject =>
@@ -19,15 +32,17 @@ const DynamicFormElement = ({ stringTag, tagContents, value, formIsInvalid, hand
       );
   	  break;
   	case 'textarea':
-  	  htmlTag = <textarea {...tagContents} value={value} onChange={handleOnChange} />;
+  	  htmlTag = <textarea className={tagClassesArray.join(' ')} {...tagContents} value={value} onChange={handleOnChange} />;
   	  break;
   	default:
-  	  htmlTag = <input {...tagContents} value={value} onChange={handleOnChange} />;
+  	  htmlTag = <input className={tagClassesArray.join(' ')} {...tagContents} value={value} onChange={handleOnChange} />;
   }
 
   return (
     <div className={styles.inputWrapper}>
+      <label className={styles.label}>{labelText}:&nbsp;</label>
       {htmlTag}
+      {validationErrorMessage}
     </div>
   )
 }
