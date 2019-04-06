3e6576058dc4d49ef7c7010da9a14785253659f0
github username <github email address>
2019-04-06 13:18:23 -0400

    remove ; in validationLogic.js; add if logic for numberIsRequired

--- client/src/utils/validationLogic.js
+++ client/src/utils/validationLogic.js
@@ -2,7 +2,11 @@ export const meetsValidationCriteria = (currentValue, criteria) => {
   let criteriaMet = true;
 
   if (criteria.cannotBeBlank) {
-    criteriaMet = currentValue.trim().length && criteriaMet;
+    criteriaMet = currentValue.trim().length && criteriaMet
+  }
+
+  if (criteria.numberIsRequired) {
+    criteriaMet = /^\d+$/.test(currentValue) && criteriaMet
   }
 
   return criteriaMet;
