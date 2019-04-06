952211a26c2ecf83094c1b42216685352606f27d
github username <github email address>
2019-04-05 20:52:02 -0400

    import CakeCustomizerForm; add /pastry-particulars route

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import CakeConstructor from './containers/CakeConstructor/CakeConstructor';
+import CakeCustomizerForm from './containers/CakeCustomizerForm';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div>
         <Navbar />
         <Switch>
+          <Route exact path="/pastry-particulars" component={CakeCustomizerForm} />
           <Route exact path="/" component={CakeConstructor} />
         </Switch>
       </div>
@@ -17,3 +19,6 @@ class App extends Component {
 }
 
 export default App;
+
+/* Components like CakeConstructor that are directly loaded through a Route component will receive match and history props 
+(Components nested inside CakeConstructor will not receive those props unless we pass them on manually or use withRouter) */
