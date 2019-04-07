921029e100fcf8562cae4fbd2019d1e4913d5852
github username <github email address>
2019-04-07 19:31:31 -0400

    add comment about cakes: state.cakes.concat(action.cake)

--- client/src/reducers/pastryPurchaser.js
+++ client/src/reducers/pastryPurchaser.js
@@ -10,7 +10,7 @@ export default (state = initialState, action) => {
     case types.PURCHASE_PASTRY_SUCCESS:
       return { // return new, updated state object
         ...state, // copy over all key/value pairs from the old, previous, existing state objects
-        cakes: [...state.cakes, action.cake] // set cakes = to a new array to maintain immutability. Copy over all cake object elements from the previous cakes array, and then push the cake object just added (received as the payload in the action dispatched) onto the end of this new array
+        cakes: [...state.cakes, action.cake] // set cakes = to a new array to maintain immutability. Copy over all cake object elements from the previous cakes array, and then push the cake object just added (received as the payload in the action dispatched) onto the end of this new array. An alternative way of writing this -- cakes: state.cakes.concat(action.cake)
       };
     case types.PURCHASE_PASTRY_FAILURE:
       return {
