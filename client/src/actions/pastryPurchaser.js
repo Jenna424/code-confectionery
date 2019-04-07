a32a83efcd7fd71455ac131816ca80c28315de0a
github username <github email address>
2019-04-07 15:04:08 -0400

    add purchasePastrySuccess & purchasePastryFailure action creators

--- client/src/actions/pastryPurchaser.js
+++ client/src/actions/pastryPurchaser.js
@@ -1,3 +1,14 @@
 import * as types from './actionTypes';
 
 const BASE_URL = process.env.REACT_APP_API_URL;
+
+// Synchronous action creator functions:
+export const purchasePastrySuccess = pastry => ({
+  type: types.PURCHASE_PASTRY_SUCCESS,
+  pastry
+})
+
+export const purchasePastryFailure = error => ({
+  type: types.PURCHASE_PASTRY_FAILURE,
+  error
+})
