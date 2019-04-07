028e4e263b0f386fc524ee074ef22236c79a659b
github username <github email address>
2019-04-07 15:40:57 -0400

    replace has_many :layers w/ layer_ids

--- app/serializers/cake_serializer.rb
+++ app/serializers/cake_serializer.rb
@@ -1,4 +1,7 @@
 class CakeSerializer < ActiveModel::Serializer
-  attributes :id, :structure, :occasion, :diameter, :price, :customer, :name, :message, :garnish, :toppers
-  has_many :layers
+  attributes :id, :structure, :occasion, :diameter, :price, :customer, :name, :message, :garnish, :toppers, :layer_ids
+
+  def layer_ids
+    object.layers.map(&:id)
+  end
 end
