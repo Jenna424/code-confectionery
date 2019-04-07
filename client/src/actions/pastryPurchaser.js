d91e1c1ac99df3ea57e32624b52105a6372039a1
github username <github email address>
2019-04-07 15:12:01 -0400

    define createCake asynchronous action creator function

--- client/src/actions/pastryPurchaser.js
+++ client/src/actions/pastryPurchaser.js
@@ -11,3 +11,21 @@ export const purchasePastryFailure = error => ({
   type: types.PURCHASE_PASTRY_FAILURE,
   error
 })
+// Asynchronous action creator function:
+export const createCake = cake => {
+  return dispatch => {
+    return fetch(`${BASE_URL}/cakes`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ cake: cake })
+    })
+      .then(response => response.json())
+      .then(cake => {
+        dispatch(purchasePastrySuccess(cake))
+        console.log(cake)
+      })
+      .catch(error => dispatch(purchasePastryFailure(error)))
+  }
+}
