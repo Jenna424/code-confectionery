6648abb3a978385f1e79630d82b7db58e2788ea7
github username <github email address>
2019-04-07 10:59:21 -0400

    edit whitelisted attributes in CakeSerializer

--- app/serializers/cake_serializer.rb
+++ app/serializers/cake_serializer.rb
@@ -1,5 +1,5 @@
 class CakeSerializer < ActiveModel::Serializer
-  attributes :id, :structure, :occasion, :width, :price, :img_url, :ordered, :flavor_combination, :ingredient_ids
+  attributes :id, :structure, :occasion, :diameter, :price, :customer, :name, :message, :garnish, :toppers, :ingredient_ids
 
   def ingredient_ids
     object.ingredients.map(&:id)
