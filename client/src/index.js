ebf5ceeeefa531181cfe5a5058e6b9b7a5206974
github username <github email address>
2019-04-07 12:47:04 -0400

    edit store by calling composeEnhancers(applyMiddleware(thunk))

--- client/src/index.js
+++ client/src/index.js
@@ -19,8 +19,7 @@ const rootReducer = combineReducers({
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(thunk)
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
