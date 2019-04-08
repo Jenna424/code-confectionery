fa7b71c7c12501b34dfcc9e4d040e13580cd495a
github username <github email address>
2019-04-08 18:43:01 -0400

    change CakesGallery.js to CakesMenu.js;replace Gallery with Menu

--- client/src/components/CakesMenu/CakesMenu.js
+++ client/src/components/CakesMenu/CakesMenu.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import styles from './CakesGallery.module.css';
+import styles from './CakesMenu.module.css';
 import CakeMenuItem from '../CakeMenuItem/CakeMenuItem';
 
-const CakesGallery = ({ cakes }) => {
+const CakesMenu = ({ cakes }) => {
   return (
     <Fragment>
       {!cakes.length &&
@@ -28,4 +28,4 @@ const CakesGallery = ({ cakes }) => {
   )
 }
 
-export default CakesGallery;
+export default CakesMenu;
