6862985e8ea039f8f3f7f1ddd4d630c35cce55a9
github username <github email address>
2019-04-08 13:12:23 -0400

    add semicolon after returned object in types.SET_CAKES_SUCCESS

--- client/src/reducers/pastryPurchaser.js
+++ client/src/reducers/pastryPurchaser.js
@@ -23,7 +23,7 @@ export default (state = initialState, action) => {
         ...state,
         cakes: action.cakes,
         error: false // set error to false to clear it in case I previously got an error that has since been resolved
-      }
+      };
     case types.FETCH_CAKES_FAILURE:
       return {
         ...state,
