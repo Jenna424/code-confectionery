7b83bc01097a53c910273070e374efc6acf9dc14
github username <github email address>
2019-04-08 11:54:56 -0400

    stub out Cakes container class component

---
+++ client/src/containers/Cakes/Cakes.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Cakes extends Component {
+  render() {
+    return (
+      <div>
+        In the componentDidMount lifecycle method of Cakes container class component, I will fetch the index of cakes from my Rails API backend.
+      </div>
+    )
+  }
+}
+
+export default Cakes;
