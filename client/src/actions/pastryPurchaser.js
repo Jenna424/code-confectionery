b52a490dc0fe8836c1ea84602d4849ef61de8a9d
github username <github email address>
2019-04-08 12:33:08 -0400

    define loadCakes asynchronous action creator in pastryPurchaser.js

--- client/src/actions/pastryPurchaser.js
+++ client/src/actions/pastryPurchaser.js
@@ -40,3 +40,12 @@ export const setCakesSuccess = cakes => ({
 export const fetchCakesFailure = () => ({
   type: types.FETCH_CAKES_FAILURE
 })
+// loadCakes is an asynchronous action creator function, made possible by redux-thunk, that handles the process of fetching cakes from my Rails API backend server
+export const loadCakes = () => {
+  return dispatch => {
+    return fetch(`${BASE_URL}/cakes`) // make async call to fetch cakes from my Rails API backend server
+      .then(response => response.json()) // handle promises. Once promises are resolved, after the async code is done and I've gotten a successful JSON response back from my server, which contains the array of cake objects,
+      .then(cakes => dispatch(setCakesSuccess(cakes))) // dispatch the action object that is returned by calling synchronous action creator function setCakesSuccess with this array of cake objects as its argument.
+      .catch(error => dispatch(fetchCakesFailure())) // However, if my request to fetch cakes from my Rails server failed, I want to adjust my error key = true in Redux store state. To this end, I dispatch the action object that is returned by calling synchronous action creator function fetchCakesFailure()
+  };
+};
