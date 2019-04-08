a232230bd9b00718ca666262ca8a72199d0ce2ea
github username <github email address>
2019-04-08 17:22:10 -0400

    define stateless functional, presentational CakeGallery component

---
+++ client/src/components/CakeGallery/CakeGallery.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CakeGallery = props => {
+  return (
+    <div>
+      This stateless functional presentational component should display the index of all cakes
+    </div>
+  )
+}
+
+export default CakeGallery;
