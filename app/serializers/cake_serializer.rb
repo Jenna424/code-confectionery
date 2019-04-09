3cd11691908ed5ed4793ef89f86d67d122679e99
github username <github email address>
2019-04-08 22:34:23 -0400

    delete CakeSerializer

--- app/serializers/cake_serializer.rb
+++
@@ -1,7 +0,0 @@
-class CakeSerializer < ActiveModel::Serializer
-  attributes :id, :structure, :occasion, :flavor_combination, :diameter, :price, :customer, :name, :message, :garnish, :toppers, :layer_ids
-
-  def layer_ids
-    object.layers.map(&:id)
-  end
-end
