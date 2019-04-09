0b44a6a5777aa923606887db3406c4caad500fbb
github username <github email address>
2019-04-08 22:15:46 -0400

    change

--- client/src/components/CakesMenu/CakesMenu.js
+++ client/src/components/CakesMenu/CakesMenu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+mport React from 'react';
 import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './CakesMenu.module.css';
@@ -9,21 +9,21 @@ const CakesMenu = ({ cakes }) => {
     <Fragment>
       {!cakes.length &&
         <div className={styles.emptyMenu}>
-      	  <p>No cakes have been customized.</p>
-      	  <p>You may design a specialty cake for our menu <Link to='/'>here</Link>.</p>
-      	</div>}
+          <p>No cakes have been customized.</p>
+          <p>You may design a specialty cake for our menu <Link to='/'>here</Link>.</p>
+        </div>}
       {cakes.length &&
-      	<div className={styles.cakesMenu}>
-      	  {cakes.map(cakeObject =>
-      	    <CakeMenuItem
-      	      key={cakeObject.id}
-      	      id={cakeObject.id}
-      	      name={cakeObject.name}
-      	      flavorCombination={cakeObject.flavor_combination}
-      	      price={cakeObject.price}
-      	    />
-      	  )}
-      	</div>}
+        <div className={styles.cakesMenu}>
+          {cakes.map(cakeObject =>
+            <CakeMenuItem
+              key={cakeObject.id}
+              id={cakeObject.id}
+              name={cakeObject.name}
+              flavorCombination={cakeObject.flavor_combination}
+              price={cakeObject.price}
+            />
+          )}
+        </div>}
     </Fragment>
   )
 }
