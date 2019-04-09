6d8ea9cf2af6c5ae989a95aff0d44ff739a3e073
github username <github email address>
2019-04-08 22:22:07 -0400

    whitelist :flavor_combination attribute in CakeSerializer

--- app/serializers/cake_serializer.rb
+++ app/serializers/cake_serializer.rb
@@ -1,5 +1,5 @@
 class CakeSerializer < ActiveModel::Serializer
-  attributes :id, :structure, :occasion, :diameter, :price, :customer, :name, :message, :garnish, :toppers, :layer_ids
+  attributes :id, :structure, :occasion, :flavor_combination, :diameter, :price, :customer, :name, :message, :garnish, :toppers, :layer_ids
 
   def layer_ids
     object.layers.map(&:id)
