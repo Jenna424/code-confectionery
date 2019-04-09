301010d39944d17212d676f85c6643a1a48f0ec5
github username <github email address>
2019-04-09 19:47:20 -0400

    delete loading from initialState and switch case statements

--- client/src/reducers/pastryPurchaser.js
+++ client/src/reducers/pastryPurchaser.js
@@ -3,8 +3,7 @@ import * as types from '../actions/actionTypes';
 const initialState = {
   cakes: [],
   cakeCreationCompleted: false,
-  error: false,
-  loading: true
+  error: false
 }
 
 export default (state = initialState, action) => {
@@ -14,27 +13,23 @@ export default (state = initialState, action) => {
         ...state, // copy over all key/value pairs from the old, previous, existing state objects
         cakes: [...state.cakes, action.cake], // set cakes = to a new array to maintain immutability. Copy over all cake object elements from the previous cakes array, and then push the cake object just added (received as the payload in the action dispatched) onto the end of this new array. An alternative way of writing this -- cakes: state.cakes.concat(action.cake)
         cakeCreationCompleted: true,
-        error: false, // set error to false to clear it in case I previously got an error that has since been resolved,
-        loading: false
+        error: false // set error to false to clear it in case I previously got an error that has since been resolved
       };
     case types.PURCHASE_PASTRY_FAILURE:
       return {
         ...state,
-        error: true,
-        loading: false
+        error: true
       };
     case types.SET_CAKES_SUCCESS:
       return {
         ...state,
         cakes: action.cakes,
-        error: false, // set error to false to clear it in case I previously got an error that has since been resolved,
-        loading: false
+        error: false // set error to false to clear it in case I previously got an error that has since been resolved,
       };
     case types.FETCH_CAKES_FAILURE:
       return {
         ...state,
-        error: true,
-        loading: false
+        error: true
       };
     default:
       return state;
