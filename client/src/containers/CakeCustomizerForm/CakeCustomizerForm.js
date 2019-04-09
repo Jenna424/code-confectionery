f7c45eaff3a3f44fcbabecaf65911b50f7125091
github username <github email address>
2019-04-09 14:12:37 -0400

    delete Redirect import statement in CakeCustomizerForm

--- client/src/containers/CakeCustomizerForm/CakeCustomizerForm.js
+++ client/src/containers/CakeCustomizerForm/CakeCustomizerForm.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
 import { meetsValidationCriteria } from '../../utils/logic.js';
 import DynamicFormElement from '../../components/shared/DynamicFormElement/DynamicFormElement';
 import { createCake } from '../../actions/pastryPurchaser';
