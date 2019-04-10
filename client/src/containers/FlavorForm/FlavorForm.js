6b44ba073bc90d1fb0903104156f58bf64c2857c
github username <github email address>
2019-04-10 07:12:14 -0400

    stub out FlavorForm container class component

---
+++ client/src/containers/FlavorForm/FlavorForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FlavorForm extends Component {
+  render() {
+  	return (
+  	  <div>
+  	    <h2>Propose a New Flavor for a Cake Layer</h2>
+  	  </div>
+  	)
+  }
+}
+
+export default FlavorForm;
