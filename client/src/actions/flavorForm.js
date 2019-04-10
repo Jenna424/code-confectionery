d4dd81206f87fa10a5c93fafe8c6b98d89323c66
github username <github email address>
2019-04-10 11:21:58 -0400

    add createFlavorSuccess/Failure synchronous action creators

--- client/src/actions/flavorForm.js
+++ client/src/actions/flavorForm.js
@@ -1,2 +1,13 @@
 import * as types from './actionTypes';
 const BASE_URL = process.env.REACT_APP_API_URL;
+
+// Synchronous action creator functions that handle the process of creating a new flavored layer:
+export const createFlavorSuccess = flavoredLayer => ({
+  type: types.CREATE_FLAVOR_SUCCESS,
+  flavoredLayer
+})
+
+export const createFlavorFailure = error => ({
+  type: types.CREATE_FLAVOR_FAILURE,
+  error
+})
