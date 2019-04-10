d7e9aa3a25abfbbf04c6db82b7a700352254befd
github username <github email address>
2019-04-10 07:53:24 -0400

    add <form> containing <div> w/ <label> & <input> for adding new flavor

--- client/src/containers/FlavorForm/FlavorForm.js
+++ client/src/containers/FlavorForm/FlavorForm.js
@@ -5,6 +5,18 @@ class FlavorForm extends Component {
     return (
       <div>
         <h2>Propose a New Flavor for a Cake Layer</h2>
+        <form>
+          <div>
+            <label htmlFor="flavor">Flavor:</label>
+            <input 
+              type="text"
+              name="flavor"
+              placeholder="Fill in this field with a new flavor for a cake layer..." 
+              onChange={this.handleOnChange} 
+              value={flavor}
+            />
+          </div>
+        </form>
       </div>
     )
   }
