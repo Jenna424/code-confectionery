f93b3495c220e6cbde361b4a6e75ce10897eb4c4
github username <github email address>
2019-04-10 15:38:24 -0400

    move updateFlavorFormData action creator above resetFlavorForm

--- client/src/actions/flavorForm.js
+++ client/src/actions/flavorForm.js
@@ -1,6 +1,11 @@
 import * as types from './actionTypes';
 const BASE_URL = process.env.REACT_APP_API_URL;
 
+export const updateFlavorFormData = flavorFormData => ({
+  type: types.UPDATE_FLAVOR_FORM_DATA,
+  flavorFormData
+})
+
 export const resetFlavorForm = () => ({
   type: types.RESET_FLAVOR_FORM
 })
@@ -23,8 +28,3 @@ export const createFlavoredLayer = layer => {
       .catch(error => dispatch(createFlavorFailure(error)))
   }
 }
-
-export const updateFlavorFormData = flavorFormData => ({
-  type: types.UPDATE_FLAVOR_FORM_DATA,
-  flavorFormData
-})
