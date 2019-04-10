3efbf452ae33b09b9ed93f4a51d6980efcc96a5d
github username <github email address>
2019-04-09 21:00:59 -0400

    change 'Container' to 'Menu' in App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import CakeConstructor from './containers/CakeConstructor/CakeConstructor';
 import CakeCustomizerForm from './containers/CakeCustomizerForm/CakeCustomizerForm';
-import CakesContainer from './containers/CakesContainer/CakesContainer';
+import CakesMenu from './containers/CakesMenu/CakesMenu';
 
 class App extends Component {
   render() {
@@ -13,7 +13,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/pastry-particulars" component={CakeCustomizerForm} />
           <Route exact path="/" component={CakeConstructor} />
-          <Route exact path="/cakes" component={CakesContainer} />
+          <Route exact path="/cakes" component={CakesMenu} />
         </Switch>
       </div>
     )
