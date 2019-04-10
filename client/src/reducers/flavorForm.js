ddbab4d604605d77f8fe86ccfc075a62746c4e0b
github username <github email address>
2019-04-10 15:31:59 -0400

    edit switch statement w/ cases for updating & resetting form

--- client/src/reducers/flavorForm.js
+++ client/src/reducers/flavorForm.js
@@ -7,6 +7,10 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case types.UPDATE_FLAVOR_FORM_DATA:
+      return action.flavorFormData;
+    case types.RESET_FLAVOR_FORM:
+      return initialState;
     default:
       return state;
   }
