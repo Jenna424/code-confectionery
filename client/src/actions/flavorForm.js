5214869c4bf4a6b6b01b9a257b8fcf9f04917c87
github username <github email address>
2019-04-10 15:43:54 -0400

    delete createFlavoredLayer async action creator from flavorForm.js

--- client/src/actions/flavorForm.js
+++ client/src/actions/flavorForm.js
@@ -9,22 +9,3 @@ export const updateFlavorFormData = flavorFormData => ({
 export const resetFlavorForm = () => ({
   type: types.RESET_FLAVOR_FORM
 })
-
-// Asynchronous action creator function that handles the process of creating a new flavored layer:
-export const createFlavoredLayer = layer => {
-  return dispatch => {
-    return fetch(`${BASE_URL}/layers`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ layer: layer })
-    })
-      .then(response => response.json())
-      .then(layer => {
-        dispatch(createFlavorSuccess(layer))
-        dispatch(resetFlavorForm())
-      })
-      .catch(error => dispatch(createFlavorFailure(error)))
-  }
-}
