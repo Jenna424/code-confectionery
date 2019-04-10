69f12552e7d57c0c60963763dadd12b6254ed3fd
github username <github email address>
2019-04-10 11:27:10 -0400

    define async action creator createFlavoredLayer

--- client/src/actions/flavorForm.js
+++ client/src/actions/flavorForm.js
@@ -11,3 +11,22 @@ export const createFlavorFailure = error => ({
   type: types.CREATE_FLAVOR_FAILURE,
   error
 })
+
+// Asynchronous action creator function that handles the process of creating a new flavored layer:
+export const createFlavoredLayer = layer => {
+  return dispatch => {
+    return fetch(`${BASE_URL}/layers`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ layer: layer })
+    })
+      .then(response => response.json())
+      .then(layer => {
+        dispatch(createFlavorSuccess(layer))
+        dispatch(resetFlavorForm())
+      })
+      .catch(error => dispatch(createFlavorFailure(error)))
+  }
+}
