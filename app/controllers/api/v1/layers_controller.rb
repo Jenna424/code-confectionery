433801cdb6baba51b8473cfd7a5cea3021127176
github username <github email address>
2019-04-10 20:06:16 -0400

    define #create action in LayersController

--- app/controllers/api/v1/layers_controller.rb
+++ app/controllers/api/v1/layers_controller.rb
@@ -1,6 +1,15 @@
 module Api
   module V1
     class LayersController < ApplicationController
+      def create
+        layer = Layer.new(layer_params)
+        if layer.save
+          render json: layer, status: 201 # indicates successful resource creation
+        else
+          render json: { errors: layer.errors.full_messages }, status: :unprocessable_entity # status: 422
+        end
+      end
+
       def index
         render json: Layer.all
       end
