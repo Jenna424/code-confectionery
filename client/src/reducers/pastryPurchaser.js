e04e58db0b09ac494927b83308ea5832adbf3fce
github username <github email address>
2019-04-10 20:51:46 -0400

    delete cakeCreationCompleted in pastryPurchaser

--- client/src/reducers/pastryPurchaser.js
+++ client/src/reducers/pastryPurchaser.js
@@ -2,7 +2,6 @@ import * as types from '../actions/actionTypes';
 
 const initialState = {
   cakes: [],
-  cakeCreationCompleted: false,
   error: false
 }
 
@@ -12,7 +11,6 @@ export default (state = initialState, action) => {
       return { // return new, updated state object
         ...state, // copy over all key/value pairs from the old, previous, existing state objects
         cakes: [...state.cakes, action.cake], // set cakes = to a new array to maintain immutability. Copy over all cake object elements from the previous cakes array, and then push the cake object just added (received as the payload in the action dispatched) onto the end of this new array. An alternative way of writing this -- cakes: state.cakes.concat(action.cake)
-        cakeCreationCompleted: true,
         error: false // set error to false to clear it in case I previously got an error that has since been resolved
       };
     case types.PURCHASE_PASTRY_FAILURE:
@@ -24,7 +22,7 @@ export default (state = initialState, action) => {
       return {
         ...state,
         cakes: [...action.cakes],
-        error: false // set error to false to clear it in case I previously got an error that has since been resolved,
+        error: false // set error to false to clear it in case I previously got an error that has since been resolved
       };
     case types.FETCH_CAKES_FAILURE:
       return {
