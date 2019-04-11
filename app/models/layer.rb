76c5f575ab2d93bf167a6f8586b6a8fb0c604a53
github username <github email address>
2019-04-10 20:10:23 -0400

    delete scope for frosting in Layer model

--- app/models/layer.rb
+++ app/models/layer.rb
@@ -1,11 +1,10 @@
 class Layer < ApplicationRecord
   scope :batter, -> { where pastry_part: 'batter' }
   scope :filling, -> { where pastry_part: 'filling' }
-  scope :frosting, -> { where pastry_part: 'frosting' }
 
   validates :flavor, presence: true, uniqueness: true
   validates :pastry_part, inclusion: { in: %w(batter filling) }
-  
+
   has_many :cake_layers
   has_many :cakes, through: :cake_layers
 end
