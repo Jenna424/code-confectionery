ca8fcf28c9087e7c0831c34093aa5a329d287258
github username <github email address>
2019-04-11 21:19:20 -0400

    add if statement logic to define fillingProportions

--- client/src/components/PastryPartsPreview/PastryPartsPreview.js
+++ client/src/components/PastryPartsPreview/PastryPartsPreview.js
@@ -4,6 +4,16 @@ import styles from './PastryPartsPreview.module.css';
 
 const PastryPartsPreview = ({ cakeBatterLayers, cakeFillingLayers, cakeCost, continueCakeCheckout, cancelCakeCheckout }) => {
   let fillingProportions;
+
+  if (!cakeFillingLayers.length) {
+    fillingProportions = <p>Your cake does not contain any filling.</p>
+  } else {
+    const fillingFlavors = Object.keys(calculateCakePartProportions(cakeFillingLayers));
+    fillingProportions = fillingFlavors.map(fillingFlavor =>
+      <p>{fillingFlavor}: {calculateCakePartProportions(cakeFillingLayers)[fillingFlavor]}</p>
+    )
+  }
+  
   return (
   )
 }
