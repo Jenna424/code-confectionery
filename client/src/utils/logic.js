8a62c28959c49c234ad860123f52ee2e113aa94e
github username <github email address>
2019-04-11 21:01:14 -0400

    define calculateCakePartProportions in logic.js

--- client/src/utils/logic.js
+++ client/src/utils/logic.js
@@ -16,3 +16,15 @@ export const dynamicallyDeleteKey = (object, keyToDelete) => {
   const {[keyToDelete]: deletedKey, ...remainingKeys} = object;
   return remainingKeys;
 }
+
+export const calculateCakePartProportions = pastryPartLayers => {
+  const flavorsMap = {};
+  pastryPartLayers.forEach(layerObject => {
+    if (!Object.keys(flavorsMap).includes(layerObject.flavor)) {
+      flavorsMap[layerObject.flavor] = 1
+    } else {
+      flavorsMap[layerObject.flavor] = flavorsMap[layerObject.flavor] + 1
+    }
+  })
+  return flavorsMap;
+}
