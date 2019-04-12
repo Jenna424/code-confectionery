134b5fc5ea19a1da08d32802270ebb98a6a6ef8a
github username <github email address>
2019-04-11 21:14:30 -0400

    stub out PastryPartsPreview w/ destructured props

--- client/src/components/PastryPartsPreview/PastryPartsPreview.js
+++ client/src/components/PastryPartsPreview/PastryPartsPreview.js
@@ -1,3 +1,10 @@
 import React, { Fragment } from 'react';
 import { calculateCakePartProportions } from '../utils/logic';
 import styles from './PastryPartsPreview.module.css';
+
+const PastryPartsPreview = ({ cakeBatterLayers, cakeFillingLayers, cakeCost, continueCakeCheckout, cancelCakeCheckout }) => {
+  return (
+  )
+}
+
+export default PastryPartsPreview;
