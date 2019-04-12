e63ebba536673711d822cec931b0d6a2e6692c29
github username <github email address>
2019-04-11 21:33:52 -0400

    edit import logic.js statement to go back 1 more folder

--- client/src/components/PastryPartsPreview/PastryPartsPreview.js
+++ client/src/components/PastryPartsPreview/PastryPartsPreview.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { calculateCakePartProportions } from '../utils/logic';
+import { calculateCakePartProportions } from '../../utils/logic';
 import styles from './PastryPartsPreview.module.css';
 
 const PastryPartsPreview = ({ cakeBatterLayers, cakeFillingLayers, cakeCost, continueCakeCheckout, cancelCakeCheckout }) => {
