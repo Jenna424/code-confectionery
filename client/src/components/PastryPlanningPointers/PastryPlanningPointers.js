602e8ec6692f03be31dc6ff44ebdce424865c364
github username <github email address>
2019-04-16 13:24:25 -0400

    add 4 <p>s to PastryPlanningPointers presentational component

--- client/src/components/PastryPlanningPointers/PastryPlanningPointers.js
+++ client/src/components/PastryPlanningPointers/PastryPlanningPointers.js
@@ -3,6 +3,10 @@ import React from 'react';
 const PastryPlanningPointers = () => (
   <div>
     <h3>Pointers for Planning Your Pastry</h3>
+    <p><em>When designing the graphical representation of your cake, please keep the following requirements in mind:</em></p>
+    <p>~ At the bare minimum, your pastry must contain at least one layer of cake batter.</p>
+    <p>~ The topmost and bottommost layers of your pastry must consist of cake batter.</p>
+    <p>~ If you are creating a multi-layered cake, you must alternate layers of batter and filling to create a stable cake structure.</p>
   </div>
 )
 
