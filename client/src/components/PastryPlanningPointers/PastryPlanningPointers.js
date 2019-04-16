82a7c318c4a11785a81211e725844c6383f7ffb9
github username <github email address>
2019-04-16 13:22:18 -0400

    remove italics from <h3> heading in PastryPlanningPointers

--- client/src/components/PastryPlanningPointers/PastryPlanningPointers.js
+++ client/src/components/PastryPlanningPointers/PastryPlanningPointers.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PastryPlanningPointers = () => (
   <div>
-    <h3><em>Pointers for Planning Your Pastry</em></h3>
+    <h3>Pointers for Planning Your Pastry</h3>
   </div>
 )
 
