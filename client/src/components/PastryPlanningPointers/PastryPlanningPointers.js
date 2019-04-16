53bc68f506e032870209c997a41af1acc286a30f
github username <github email address>
2019-04-16 17:41:18 -0400

    add <h4> in PastryPlanningPointers JSX code

--- client/src/components/PastryPlanningPointers/PastryPlanningPointers.js
+++ client/src/components/PastryPlanningPointers/PastryPlanningPointers.js
@@ -4,6 +4,7 @@ import styles from './PastryPlanningPointers.module.css';
 const PastryPlanningPointers = () => (
   <div className={styles.pointers}>
     <h3>Pointers for Planning Your Pastry</h3>
+    <h4>When designing the graphical representation of your cake, please remember that:</h4>
   </div>
 )
 
