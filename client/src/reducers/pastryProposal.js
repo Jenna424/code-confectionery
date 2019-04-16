96ee5cc56ed65bf29ea6f49c7b0b28b15c473a5e
github username <github email address>
2019-04-16 09:16:13 -0400

    change 'PURCHASE' to 'PROPOSE' in pastryProposal reducer function

--- client/src/reducers/pastryProposal.js
+++ client/src/reducers/pastryProposal.js
@@ -8,14 +8,14 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case types.PURCHASE_PASTRY_SUCCESS:
+    case types.PROPOSE_PASTRY_SUCCESS:
       return { // return new, updated state object
         ...state, // copy over all key/value pairs from the old, previous, existing state objects
         cakes: [...state.cakes, action.cake], // set cakes = to a new array to maintain immutability. Copy over all cake object elements from the previous cakes array, and then push the cake object just added (received as the payload in the action dispatched) onto the end of this new array. An alternative way of writing this -- cakes: state.cakes.concat(action.cake)
         cakeCreationCompleted: true,
         error: false // set error to false to clear it in case I previously got an error that has since been resolved
       };
-    case types.PURCHASE_PASTRY_FAILURE:
+    case types.PROPOSE_PASTRY_FAILURE:
       return {
         ...state,
         error: true
