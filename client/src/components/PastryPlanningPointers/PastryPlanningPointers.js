6345f775333d83d75dceb1e6e6643c30cbef8caf
github username <github email address>
2019-04-16 17:42:15 -0400

    change 'graphical representation' to 'layout' in <h4>

--- client/src/components/PastryPlanningPointers/PastryPlanningPointers.js
+++ client/src/components/PastryPlanningPointers/PastryPlanningPointers.js
@@ -4,7 +4,7 @@ import styles from './PastryPlanningPointers.module.css';
 const PastryPlanningPointers = () => (
   <div className={styles.pointers}>
     <h3>Pointers for Planning Your Pastry</h3>
-    <h4>When designing the graphical representation of your cake, please remember that:</h4>
+    <h4>When designing the layout of your cake, please remember that:</h4>
   </div>
 )
 
