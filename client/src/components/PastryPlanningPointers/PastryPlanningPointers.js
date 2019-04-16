db67a9298db4c20ae01b33af51f74f55dc0e0937
github username <github email address>
2019-04-16 17:43:27 -0400

    add 3 italicized <p>s to PastryPlanningPointers

--- client/src/components/PastryPlanningPointers/PastryPlanningPointers.js
+++ client/src/components/PastryPlanningPointers/PastryPlanningPointers.js
@@ -5,6 +5,9 @@ const PastryPlanningPointers = () => (
   <div className={styles.pointers}>
     <h3>Pointers for Planning Your Pastry</h3>
     <h4>When designing the layout of your cake, please remember that:</h4>
+    <p><em>~ Your pastry must contain at least one layer of cake batter ~</em></p>
+    <p><em>~ Multilayered cakes must contain alternating layers of batter and filling ~</em></p>
+    <p><em>~ The topmost and bottommost layers must consist of batter ~</em></p>
   </div>
 )
 
