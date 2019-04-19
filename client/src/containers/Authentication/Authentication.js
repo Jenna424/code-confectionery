b1a878f4a306c36d9580d3e4e9d8b9fad0c3dff8
github username <github email address>
2019-04-19 11:03:59 -0400

    add local state object with authFormInputs key to Authentication

--- client/src/containers/Authentication/Authentication.js
+++ client/src/containers/Authentication/Authentication.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import '../../components/shared/DynamicFormElement/DynamicFormElement';
 
 class Authentication extends Component {
+  state = {
+    authFormInputs: {
+    }
+  }
+
   render() {
   	return (
   	  <div>
@@ -13,3 +18,10 @@ class Authentication extends Component {
 }
 
 export default Authentication;
+
+{/*
+// Authentication container class component renders the Sign Up / Log In form
+// I manage the form through the local state object of Authentication container class component
+// (and not through Redux) because I'm only concerned about the local state, 
+// the values that the user entered into the form fields
+*/}
