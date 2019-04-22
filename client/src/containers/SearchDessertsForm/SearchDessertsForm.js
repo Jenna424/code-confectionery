f1b23bbfcf397b1d4209588d0521ea05d60422ca
github username <github email address>
2019-04-22 13:22:08 -0400

    stub out SearchDessertsForm component; add export statement

--- client/src/containers/SearchDessertsForm/SearchDessertsForm.js
+++ client/src/containers/SearchDessertsForm/SearchDessertsForm.js
@@ -3,3 +3,14 @@ import { connect } from 'react-redux';
 import { searchDesserts } from '../../actions/searchDesserts';
 import styles from './SearchDessertsForm.module.css';
 import PastryPictures from '../../components/PastryPictures/PastryPictures';
+
+class SearchForm extends Component {
+  render() {
+    return (
+      <div className={styles.searchFormContainer}>
+      </div>
+    )
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchForm)
