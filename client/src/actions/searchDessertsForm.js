a0908567ab67b595b5afbff4a02a67bb9c292c47
github username <github email address>
2019-04-22 13:02:02 -0400

    define synchronous action creator searchDessertsFailure

--- client/src/actions/searchDessertsForm.js
+++ client/src/actions/searchDessertsForm.js
@@ -5,6 +5,11 @@ export const setSearchedDesserts = desserts => ({
   desserts
 })
 
+export const searchDessertsFailure = error => ({
+  type: types.SEARCH_DESSERTS_FAILURE,
+  error
+})
+
 export const searchDesserts = keywords => {
   return dispatch => {
     return fetch(`https://api.unsplash.com/search/photos?client_id=bb54ba9dca9f4f85fc84acc11c78ba903d1bfab32c29462b9b90ed82bf78533b&query=${keywords}`)
