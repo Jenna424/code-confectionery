85b99c521580bc2298315997b8cfc20bf25d3617
github username <github email address>
2019-04-22 13:19:54 -0400

    add 5 import statements in SearchDessertsForm container

---
+++ client/src/containers/SearchDessertsForm/SearchDessertsForm.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { searchDesserts } from '../../actions/searchDesserts';
+import styles from './SearchDessertsForm.module.css';
+import PastryPictures from '../../components/PastryPictures/PastryPictures';
