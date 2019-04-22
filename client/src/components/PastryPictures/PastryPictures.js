dbc846f6f1986adfb9b103c11cfa6cfe8e3d84e3
github username <github email address>
2019-04-22 16:55:35 -0400

    define PastryPictures stateless functional presentational component

--- client/src/components/PastryPictures/PastryPictures.js
+++ client/src/components/PastryPictures/PastryPictures.js
@@ -2,3 +2,14 @@ import React from 'react';
 import styles from './PastryPictures.module.css';
 import PastryPicture from '../PastryPicture/PastryPicture';
 
+const PastryPictures = ({ pictures }) => {
+  return (
+    <div className={styles.pastryPictures}>
+      {pictures.map(picture =>
+        <PastryPicture key={picture.id} picture={picture} />
+      )}
+    </div>
+  )
+}
+
+export default PastryPictures;
