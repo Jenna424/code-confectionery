5599e40cae28c8b1f9ae5befcb546b4ce3106029
github username <github email address>
2019-04-22 12:48:23 -0400

    define async action creator to fetch images from Unsplash API

--- client/src/actions/searchDessertsForm.js
+++ client/src/actions/searchDessertsForm.js
@@ -4,3 +4,12 @@ export const setSearchedDesserts = desserts => ({
   type: types.SET_SEARCHED_DESSERTS,
   desserts
 })
+
+export const searchDesserts = keywords => {
+  return dispatch => {
+    return fetch(`https://api.unsplash.com/search/photos?client_id=bb54ba9dca9f4f85fc84acc11c78ba903d1bfab32c29462b9b90ed82bf78533b&query=${keywords}`)
+      .then(response => response.json())
+      .then(jsonResponse => dispatch(setSearchedDesserts(jsonResponse.results)))
+      .catch(error => dispatch(searchDessertsFailure(error)))
+  }
+}
