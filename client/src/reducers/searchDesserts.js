86941b9799cf1474af802e03a740d7c49a8d143b
github username <github email address>
2019-04-22 13:10:21 -0400

    define switch statement w/ case 'SET_SEARCHED_DESSERTS'

--- client/src/reducers/searchDesserts.js
+++ client/src/reducers/searchDesserts.js
@@ -3,3 +3,16 @@ import * as types from '../actions/actionTypes';
 const initialState = {
   desserts: []
 }
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case types.SET_SEARCHED_DESSERTS:
+      return {
+        ...initialState,
+        desserts: [...action.desserts],
+        error: false
+      }
+    default:
+      return state;
+  }
+}
