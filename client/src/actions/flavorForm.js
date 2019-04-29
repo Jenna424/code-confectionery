a65bb51fb08f1fdb6f144ccc44d0a107e8ad25ff
github username <github email address>
2019-04-28 20:43:53 -0400

    delete BASE_URL constant in flavorForm action creators file

--- client/src/actions/flavorForm.js
+++ client/src/actions/flavorForm.js
@@ -1,5 +1,4 @@
 import * as types from './actionTypes';
-const BASE_URL = process.env.REACT_APP_API_URL;
 
 export const updateFlavorFormData = flavorFormData => ({
   type: types.UPDATE_FLAVOR_FORM_DATA,
